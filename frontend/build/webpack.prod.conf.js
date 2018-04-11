47ccd901016c587880ef5b18d2831cd039800b06
David Picarra <david.picarra@comcast.com>
2018-04-11 13:20:40 +0100

    chore: remove keybase.txt

--- frontend/build/webpack.prod.conf.js
+++ frontend/build/webpack.prod.conf.js
@@ -93,10 +93,6 @@ var webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
-      },
-      {
-        from: path.resolve(__dirname, '../keybase.txt'),
-        to: '.'
       }
     ])
   ]
